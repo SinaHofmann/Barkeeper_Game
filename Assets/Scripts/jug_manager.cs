154dbf43e8f6d04ccc0769cc7810d66c4306ce59
SinaHofmann <57066527+SinaHofmann@users.noreply.github.com>
2023-11-11 17:51:37 +0100

    Added new ing assets
    - new ing assets
    - ing are now prefabs

---
+++ Assets/Scripts/jug_manager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jug_manager : MonoBehaviour
+{
+    private void Update()
+    {
+        //on trigger enter ckeck if the object that is registered in the trigger is of layer Interactable
+        // if yes read the number it has and put number in list
+    }
+}
